7f969fc3597752893fdb5ac535d1c8e99e755b20
hannahchun <hannahchun2000@gmail.com>
2022-04-23 21:33:33 +0900

    load data from file

--- manager.c
+++ manager.c
@@ -51,17 +51,23 @@ void saveData(Product p[], int count){
 	printf("저장됨!\n");
 }
 
-/*
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
 	int count=0;
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+    	fp=fopen("product.txt","rt");
+    	if(fp==NULL)
+        	return 0;
+    	for(count=0 ; count<100 ; count++){
+        	fscanf(fp,"%d ",&p[count].weight);
+        	fscanf(fp,"%d ",&p[count].price);
+        	fgets(p[count].name,sizeof(p[count].name),fp);
+        	p[count].name[strlen(p[count].name)-1]='\0';
+        	if(feof(fp)) break;
+    	}
+    	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
-}*/
+}
